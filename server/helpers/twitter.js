5a6c1cb15281b01eb08906593c7e7e258dd70357
Jessica West <jgoulding2@gmail.com>
2017-10-11 20:54:14 +0200

    clean up

--- server/helpers/twitter.js
+++ server/helpers/twitter.js
@@ -4,10 +4,10 @@ module.exports = {
 
 // fetch tweets from the twitter API
 function getTweets(user, twitterClient) {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     // fetch the last 200 tweets from this user, not including retweets
-    const params = { screen_name: user.username, count: 200, include_rts: false };
-    twitterClient.get('statuses/user_timeline', params, function(error, tweets, response) {
+    var params = { screen_name: user.username, count: 200, include_rts: false };
+    twitterClient.get('statuses/user_timeline', params, function (error, tweets, response) {
       if (error) {
         reject(error);
       } else {
