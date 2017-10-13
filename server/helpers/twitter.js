afe5d24bcbb1d79bbde86f372b02297e6d21c027
Josh Dzielak <jdzielak@gmail.com>
2017-10-14 00:39:52 +0200

    Support an arbitrary number of twitter api calls back in time

--- server/helpers/twitter.js
+++ server/helpers/twitter.js
@@ -2,39 +2,46 @@ module.exports = {
   getTweets: getTweets
 };
 
-function getTweetsOlderThan(maxId, user, twitterClient) {
-  return new Promise(function (resolve, reject) {
-    var params = { screen_name: user.username, count: 200, include_rts: false };
-    if (maxId > -1) {
-      params.max_id = maxId;
-    }
-    twitterClient.get('statuses/user_timeline', params, function (error, tweets, response) {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(tweets);
-      }
-    });
-  });
-}
+// the highest amount of tweets you can ask twitter for per API call
+// the real amount you get back isn't 200 but 200 minus all retweets
+const MAX_TWEETS_PER_FETCH = 200;
+
+// how far back in the timeline to go - the hard twitter API limit is 3200
+// the real amount of tweets added to the index will be the number you
+// put here minus all retweets; chance the NUMBER_OF_TWEETS_TO_FETCH
+// environment variable to fetch more or less tweets, default is 400
+const NUMBER_OF_TWEETS_TO_FETCH = process.env.NUMBER_OF_TWEETS_TO_FETCH || 400;
 
 // fetch tweets from the twitter API
 function getTweets(user, twitterClient) {
+  const iterations = Math.ceil(NUMBER_OF_TWEETS_TO_FETCH / MAX_TWEETS_PER_FETCH);
+  return getTweetsOlderThan([], iterations, -1, user, twitterClient);
+}
+
+// recursive method that runs iterationsLeft number of times, adding tweets found
+// to an array, changing the max_id value to keep getting older tweets
+function getTweetsOlderThan(allTweets, iterationsLeft, maxTweetId, user, twitterClient) {
   return new Promise(function (resolve, reject) {
-    // fetch the last 200 tweets from this user, not including retweets
-    const allTweets = [];
-    var lastId;
-    getTweetsOlderThan(-1, user, twitterClient).then((tweets) => {
-      if (tweets.length > 0) {
-        allTweets.push(...tweets);
-        var oldestTweet = tweets[tweets.length - 1];
-        getTweetsOlderThan(oldestTweet.id_str, user, twitterClient).then((tweets) => {
-          allTweets.push(...tweets);
-          resolve(allTweets);
-        });
-      } else {
-        resolve(tweets);
+    if (iterationsLeft === 0) {
+      resolve(allTweets);
+      return;
+    }
+    var params = { screen_name: user.username, count: MAX_TWEETS_PER_FETCH, include_rts: false };
+    if (maxTweetId > -1) {
+      params.max_id = maxTweetId;
+    }
+    twitterClient.get('statuses/user_timeline', params, function (error, tweets, response) {
+      if (tweets.length === 0) {
+        resolve(allTweets);
+        return;
       }
-    })
+      allTweets.push(...tweets);
+      var oldestTweet = tweets[tweets.length - 1];
+      getTweetsOlderThan(allTweets, iterationsLeft - 1, oldestTweet.id_str, user, twitterClient).then(() => {
+        resolve(allTweets);
+      }).catch((err) => {
+        reject(err);
+      })
+    });
   });
 }
