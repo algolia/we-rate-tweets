e73a8432d51d48f741f9c5c50baf87c0a0b28f9f
Josh Dzielak <jdzielak@gmail.com>
2017-10-13 19:26:49 +0200

    Grab a second page of tweets

--- server/helpers/twitter.js
+++ server/helpers/twitter.js
@@ -2,11 +2,12 @@ module.exports = {
   getTweets: getTweets
 };
 
-// fetch tweets from the twitter API
-function getTweets(user, twitterClient) {
+function getTweetsOlderThan(maxId, user, twitterClient) {
   return new Promise(function (resolve, reject) {
-    // fetch the last 200 tweets from this user, not including retweets
     var params = { screen_name: user.username, count: 200, include_rts: false };
+    if (maxId > -1) {
+      params.max_id = maxId;
+    }
     twitterClient.get('statuses/user_timeline', params, function (error, tweets, response) {
       if (error) {
         reject(error);
@@ -14,5 +15,26 @@ function getTweets(user, twitterClient) {
         resolve(tweets);
       }
     });
+  });
+}
+
+// fetch tweets from the twitter API
+function getTweets(user, twitterClient) {
+  return new Promise(function (resolve, reject) {
+    // fetch the last 200 tweets from this user, not including retweets
+    const allTweets = [];
+    var lastId;
+    getTweetsOlderThan(-1, user, twitterClient).then((tweets) => {
+      if (tweets.length > 0) {
+        allTweets.push(...tweets);
+        var oldestTweet = tweets[tweets.length - 1];
+        getTweetsOlderThan(oldestTweet.id_str, user, twitterClient).then((tweets) => {
+          allTweets.push(...tweets);
+          resolve(allTweets);
+        });
+      } else {
+        resolve(tweets);
+      }
+    })
   });
 }
